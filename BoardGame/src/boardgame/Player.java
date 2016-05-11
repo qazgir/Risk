2f544134b253cd1a87599c6ea15a9cd89792463e
David Pan <dpan@ucls.uchicago.edu>
2016-05-11 13:29:46 -0500

    Created player class

---
+++ BoardGame/src/boardgame/Player.java
@@ -0,0 +1,52 @@
+package boardgame;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author csstudent
+ */
+public class Player {
+    private String phase;
+    private int units;
+    private String name;
+    private String territory;
+    
+    public String getPhase(){
+        return phase;
+        
+    }
+    
+    public int getUnits(){
+        
+        return units;
+    }
+    
+    public boolean canMove(){
+        if(getPhase().equals("Move")){
+            if(units <= 3){
+               return true; 
+                
+            }
+        }
+            return false;
+    }
+    
+    public void canAttack(){
+        if(getPhase().equals("attack")){
+            if(territory.equals("home") == false){
+                
+                
+            }
+            
+            
+        }
+        
+    }
+    
+    
+}
