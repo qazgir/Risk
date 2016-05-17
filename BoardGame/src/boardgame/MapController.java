332c27324498ff4b4b8bd2858200ee608974e3f6
David Pan <dpan@ucls.uchicago.edu>
2016-05-17 14:13:25 -0500

    Terratories get named

--- BoardGame/src/boardgame/MapController.java
+++ BoardGame/src/boardgame/MapController.java
@@ -10,7 +10,6 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 
@@ -20,6 +19,39 @@ import javafx.scene.text.Text;
  * @author csstudent
  */
 public class MapController implements Initializable {
+    
+@FXML
+public Button button1;
+@FXML
+public Button button2;
+@FXML
+public Button button3;
+@FXML
+public Button button4;
+@FXML
+public Button button5;
+@FXML
+public Button button6;
+@FXML
+public Button button7;
+@FXML
+public Button button8;
+@FXML
+public Button button9;
+@FXML
+public Button button10;
+@FXML
+public Button button11;
+@FXML
+public Button button12;
+@FXML
+public Button button13;
+@FXML
+public Button button14;
+@FXML
+public Button button15;
+@FXML
+public Button button16;
     
 @FXML
 public Text phaseIndic;
@@ -40,4 +72,30 @@ private ImageView background;
        
     }    
     
+    public void createMVPTerratories(){
+        Territory t1 = new Territory(0, "JackyMayo", button1);
+        Territory t2 = new Territory(0, "EE-Sama", button2);
+        Territory t3 = new Territory(0, "RTZBabyrage", button3);
+        Territory t4 = new Territory(0, "EnChuanTress", button4);
+        Territory t5 = new Territory(0, "JeraxSpirit", button5);
+        Territory t6 = new Territory(0, "ODPixel", button6);
+        Territory t7 = new Territory(0, "PEDUR", button7);
+        Territory t8 = new Territory(0, "IndianOfflaners", button8);
+        Territory t9 = new Territory(0, "brb i go buy bread", button9);
+        Territory t10 = new Territory(0, "9000MMRPOINTS", button10);
+        Territory t11 = new Territory(0, "HappyLilTree", button11);
+        Territory t12 = new Territory(0, "VirtusThrow", button12);
+        Territory t13 = new Territory(0, "DondoFace", button13);
+        Territory t14 = new Territory(0, "DC>>MemeArrows", button14);
+        Territory t15 = new Territory(0, "Keepo", button15);
+        Territory t16 = new Territory(0, "SinGSonGPinGPonG", button16);
+        Territory[] TArray1= {t1, t2, t3, t4};
+        Territory[] TArray2= {t5, t6, t7, t8};
+        Territory[] TArray3= {t9, t10, t11, t12};
+        Territory[] TArray4= {t13, t14, t15, t16};
+        Continent c1 = new Continent(4, "Continent1",TArray1);
+        Continent c2 = new Continent(4, "Continent2",TArray2);
+        Continent c3 = new Continent(4, "Continent3",TArray3);
+        Continent c4 = new Continent(4, "Continent4",TArray4);
+        }
 }
