cb782df2b0c7c4d39e09aba534fbc8f6129322d3
David Pan <dpan@ucls.uchicago.edu>
2016-05-18 13:46:18 -0500

    Re wrote the buttons

--- BoardGame/src/boardgame/MainMenuController.java
+++ BoardGame/src/boardgame/MainMenuController.java
@@ -7,13 +7,18 @@ package boardgame;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
@@ -40,6 +45,8 @@ private ImageView backgroundImage;
 private final Image riskImage = new Image("http://screenrant.com/wp-content/uploads/Risk-board-game-movie-being-scripted-by-Shield-writer.jpg", 580, 450, true, true);     
 
 public static Stage stage2;
+
+private int startingUnits;
     
 public static FXMLLoader fxmlLoader;
 
@@ -54,6 +61,35 @@ public void quit(){
 }
 
 public void startGame() throws IOException{
+    Alert alert = new Alert(AlertType.CONFIRMATION);
+    alert.setTitle("Number of Players");
+    alert.setHeaderText("Choose the players");
+    alert.setContentText("How many people are playing?");
+
+    ButtonType buttonTypeTwo = new ButtonType("Two");
+    ButtonType buttonTypeThree = new ButtonType("Three");
+    ButtonType buttonTypeFour = new ButtonType("Four");
+    ButtonType buttonTypeFive = new ButtonType("Five");
+    ButtonType buttonTypeSix = new ButtonType("Six");
+    ButtonType buttonTypeCancel = new ButtonType("Cancel", ButtonData.CANCEL_CLOSE);
+    ButtonType buttonTypeOne;
+
+    alert.getButtonTypes().setAll(buttonTypeTwo, buttonTypeThree, buttonTypeFour, buttonTypeFive, buttonTypeSix, buttonTypeCancel);
+
+    Optional<ButtonType> result = alert.showAndWait();
+    if (result.get() == buttonTypeTwo){
+        this.startingUnits = 40;
+    } else if (result.get() == buttonTypeThree) {
+       this.startingUnits = 35;
+    } else if (result.get() == buttonTypeFour) {
+        this.startingUnits = 30;
+    } else if (result.get() == buttonTypeFour) {
+        this.startingUnits = 25;
+    }else if (result.get() == buttonTypeFour) {
+        this.startingUnits = 20;
+    }else if (result.get() == buttonTypeCancel){
+        System.exit(0);
+    }
     MainMenuController.stage2 = BoardGame.stage;
     fxmlLoader = new FXMLLoader(getClass().getResource("Map.fxml"));  
     Parent root = fxmlLoader.load();
