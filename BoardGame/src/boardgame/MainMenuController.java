dde4f5cc085425131d531d968b2ec122c0fea47a
David Pan <dpan@ucls.uchicago.edu>
2016-05-18 13:20:58 -0500

    Added an alert asking for number of players

--- BoardGame/src/boardgame/MainMenuController.java
+++ BoardGame/src/boardgame/MainMenuController.java
@@ -7,13 +7,17 @@ package boardgame;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
@@ -43,6 +47,8 @@ public static Stage stage2;
     
 public static FXMLLoader fxmlLoader;
 
+private int startingUnits;
+
 @Override
 public void initialize(URL url, ResourceBundle rb) {
     BoardGame.stage.setResizable(false);
@@ -63,6 +69,31 @@ public void startGame() throws IOException{
     MapController terrtoryCreater = fxmlLoader.getController();
     terrtoryCreater.createMVPTerratories();
     stage2.show();
+    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Number of Players");
+        alert.setHeaderText("Player Selector");
+        alert.setContentText("How many players do you want?");
+
+        ButtonType buttonTypeThree = new ButtonType("Three");
+        ButtonType buttonTypeFour = new ButtonType("Four");
+        ButtonType buttonTypeFive = new ButtonType("Five");
+        ButtonType buttonTypeSix = new ButtonType("six");
+        ButtonType buttonTypeCancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+
+        alert.getButtonTypes().setAll(buttonTypeThree, buttonTypeFour, buttonTypeFive, buttonTypeSix, buttonTypeCancel);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == buttonTypeThree){
+            this.startingUnits = 35;
+        } else if (result.get() == buttonTypeFour) {
+            this.startingUnits = 30;
+        } else if (result.get() == buttonTypeFive) {
+            this.startingUnits = 25;
+        } else if (result.get() == buttonTypeSix){
+            this.startingUnits = 20;
+        }else{
+            // ... user chose CANCEL or closed the dialog
+        }
     
 }
 
